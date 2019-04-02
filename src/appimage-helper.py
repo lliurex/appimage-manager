2c8231000807b34a63a39ccccdd1e1f7e45cce3c
juanma1980 <juanma1980@gmail.com>
2019-04-02 17:51:13 +0200

    Wip on manager

--- src/appimage-helper.py
+++ src/appimage-helper.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 
 err=0
-dbg=False
+dbg=True
 
 def _debug(msg):
 	if dbg:
@@ -20,22 +20,18 @@ if sys.argv[1]=='install':
 	#install app
 	shutil.copy2(appimage,path)
 	#Generate desktop
-	f_desktop="[Desktop Entry]\nVersion=1.0\nEncoding=UTF-8\nType=Application\n"
-	for key,value in desktop.items():
-		f_desktop+="%s=%s\n"%(key,value)
 	try:
-		_debug("/usr/share/applications/%s.desktop"%desktop['Name'])
-		with open ("/usr/share/applications/%s.desktop"%desktop['Name'],'w') as f:
-			f.writelines(f_desktop)
+		with open ("/usr/share/applications/%s.desktop"%os.path.basename(appimage),'w') as f:
+			f.writelines(desktop)
 	except Exception as e:
-		retval=False
+		err=1
 		_debug(e)
-	return retval
+
 #def _generate_desktop
 	try:
 		subprocess.check_call(['gtk-update-icon-cache','/usr/share/icons/hicolor/'])
 	except:
-		err=1
+		err=2
 elif sys.argv[1]=='run':
 	appimage=sys.argv[2]
 	os.chmod(appimage,0o744)
