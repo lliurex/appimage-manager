b848f1fb26d5d1fc2027583d34e405e3920ede0f
lliurex <juanma1980@gmail.com>
2026-08-31 11:27:40 +0200

    PySide6

--- src/stacks/installApp.py
+++ src/stacks/installApp.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import os
-from PySide2.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QVBoxLayout,QLineEdit,QGridLayout,QHBoxLayout,QComboBox,QCheckBox, QListWidget,QFileDialog,QFrame
-from PySide2 import QtGui
-from PySide2.QtCore import Qt,QSize
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QPushButton,QVBoxLayout,QLineEdit,QGridLayout,QHBoxLayout,QComboBox,QCheckBox, QListWidget,QFileDialog,QFrame
+from PySide6 import QtGui
+from PySide6.QtCore import Qt,QSize
 from QtExtraWidgets import QStackedWindowItem
 from stacks.lib.libappmanager import appmanager as appmanager
 
