e7359115bb5945190fe834bf9c2c4ed153a5ca07
lliurex <juanma1980@gmail.com>
2026-09-10 08:33:59 +0200

    split chains

---
+++ src/stacks/extras/i18n.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import gettext
+_ = gettext.gettext
+
+i18n={"APP_UNINSTALLED":_("Uninstalled: "),
+	"APPLAUNCH":_("Launch"),
+	"APPREMOVE":_("Remove"),
+	"ERR_NOAPP":_("There're no appimages availables"),
+	"MANAGER_MENU":_("Manage"),
+	"MANAGER_MENU_DESC":_("Manage appimages"),
+	"MANAGER_MENU_TOOLTIP":_("Manage installed appimages"),
+	"MANAGER_TOOLTIP":_("Add custom repositories")
+	}
+
+
